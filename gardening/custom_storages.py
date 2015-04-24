6de42cfd96d14a36cd7892d7337b9cd0d2266868
paolopaolopaolo <dpaolomercado@gmail.com>
2015-04-23 17:53:30 -0700

    Site Deployment Fixes:
    - STATIC_ROOT setting added
    - custom_storages.py module added
    - Fixed settings.py StaticFiles/CompressedFiles storage issues
    - Included a missing css file

---
+++ gardening/custom_storages.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from storages.backends.s3boto import S3BotoStorage
+from django.core.files.storage import get_storage_class
+
+class StaticStorage(S3BotoStorage):
+    """
+    S3 storage backend that saves the files locally, too.
+    """
+    location = "static"
+
+    def __init__(self, *args, **kwargs):
+        super(StaticStorage, self).__init__(*args, **kwargs)
+        self.local_storage = get_storage_class(
+            "compressor.storage.CompressorFileStorage")()
+
+    def save(self, name, content):
+        name = super(StaticStorage, self).save(name, content)
+        self.local_storage._save(name, content)
+        return name
+
+class MediaStorage(S3BotoStorage):
+    location = "media"
+    def __init__(self, *args, **kwargs):
+        super(MediaStorage, self).__init__(*args, **kwargs)
+        self.local_storage = get_storage_class(
+            "compressor.storage.CompressorFileStorage")()
+
+    def save(self, name, content):
+        name = super(MediaStorage, self).save(name, content)
+        self.local_storage._save(name, content)
+        return name
+
+class CacheStorage(S3BotoStorage):
+    location = settings.COMPRESS_LOCATION
