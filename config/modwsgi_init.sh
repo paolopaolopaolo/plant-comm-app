8f0f504e5947f58a005dfcbade184457b0f35d88
paolopaolopaolo <dpaolomercado@gmail.com>
2015-08-26 15:31:21 -0700

    Configuration Fix:
    - added group and user to modwsgi call
    - doesn't work otherwise

--- config/modwsgi_init.sh
+++ config/modwsgi_init.sh
@@ -2,4 +2,4 @@
 . ~/.bash_profile
 . $PLANT_ENV_PATH/bin/activate
 cd $PLANT_APP_PATH
-python manage.py runmodwsgi --port=8000 --processes=2 --threads=15
+python manage.py runmodwsgi --user ec2-user --group www-data --port=8000 --processes=2 --threads=15
